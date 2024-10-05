5f8056d5d4c3bcc02b977f9c13134382f2ec7ebd
skuhlmann <samkuhlmann@gmail.com>
2024-10-05 16:14:05 -0600

    handle env

--- src/ui.ts
+++ src/ui.ts
@@ -1,4 +1,4 @@
-import { createSystem } from 'frog/ui';
+import { createSystem } from "frog/ui";
 
 export const {
   Box,
@@ -14,23 +14,23 @@ export const {
   vars,
 } = createSystem({
   colors: {
-    nipple: '#F098C6',
-    diarrhea: '#655C31',
-    jaundice: '#FEDB1D',
-    banality: '#F9E1A3',
-    death: '#000000',
-    angel: '#FFFFFF',
+    nipple: "#F098C6",
+    diarrhea: "#655C31",
+    jaundice: "#FEDB1D",
+    banality: "#F9E1A3",
+    death: "#000000",
+    angel: "#FFFFFF",
   },
   fonts: {
     default: [
       {
-        name: 'Poppins',
-        source: 'google',
+        name: "Poppins",
+        source: "google",
         weight: 900,
       },
       {
-        name: 'Poppins',
-        source: 'google',
+        name: "Poppins",
+        source: "google",
         weight: 400,
       },
     ],
